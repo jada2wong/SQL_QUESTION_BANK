8b2303e155dd771b4898defd57651e763153a8e9
Jada Wang <36458522+jada2wong@users.noreply.github.com>
2020-08-17 23:09:28 -0400

    Update 2020_08_17.sql

--- 2020_08_17.sql
+++ 2020_08_17.sql
@@ -1,1 +1,17 @@
 
+-- Write a query in SQL to obtain the nurses and the block where
+-- they are booked for attending the patients on call 
+
+-- Write a query in SQL to obtain the nurses and the block where
+-- they are booked for attending the patients on call 
+
+-- Table: nurse 
+-- employeed_id |      name       |    position  | registered | ssn
+--    101       | Carla Espinosa  | Head Nurse   | True       | 110*****
+-- Table: stay
+-- stay_id |  patient  |   room  |   start_date   | end_date
+--  3251   | 10000001  |   111   |   2018-05-01   | 2018-05-04
+
+SELECT name, employeed_id
+FROM nurse
+WHERE registered = 'True'
